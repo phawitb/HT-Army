7e23e761d85a116e5c643ecd9a56b51d4c214518
phawitb <96680592+phawitb@users.noreply.github.com>
2022-12-04 23:43:47 +0700

    Add files via upload

---
+++ App/public/scripts/index.js
@@ -0,0 +1,436 @@
+// convert epochtime to JavaScripte Date object
+function epochToJsDate(epochTime){
+    return new Date(epochTime*1000);
+  }
+
+  // convert time to human-readable format YYYY/MM/DD HH:MM:SS
+  function epochToDateTime(epochTime){
+    var epochDate = new Date(epochToJsDate(epochTime));
+    var dateTime = epochDate.getFullYear() + "/" +
+      ("00" + (epochDate.getMonth() + 1)).slice(-2) + "/" +
+      ("00" + epochDate.getDate()).slice(-2) + " " +
+      ("00" + epochDate.getHours()).slice(-2) + ":" +
+      ("00" + epochDate.getMinutes()).slice(-2) + ":" +
+      ("00" + epochDate.getSeconds()).slice(-2);
+  
+    return dateTime;
+  }
+  
+  // function to plot values on charts
+  function plotValues(chart, timestamp, value){
+    console.log(timestamp);
+    var x = epochToJsDate(timestamp).getTime() + 3600000*7;   //shiptime zone +7
+    // x += 3600000;
+    // console.log(x);
+    // console.log(x+3600000);
+    var y = Number (value);
+    if(chart.series[0].data.length > 40) {
+      chart.series[0].addPoint([x, y], true, true, true);
+    } else {
+      chart.series[0].addPoint([x, y], true, false, true);
+    }
+  }
+  
+  // DOM elements
+  const loginElement = document.querySelector('#login-form');
+  const contentElement = document.querySelector("#content-sign-in");
+  const userDetailsElement = document.querySelector('#user-details');
+  const authBarElement = document.querySelector('#authentication-bar');
+//   const deleteButtonElement = document.getElementById('delete-button');
+  const deleteModalElement = document.getElementById('delete-modal');
+  const deleteDataFormElement = document.querySelector('#delete-data-form');
+  const viewDataButtonElement = document.getElementById('view-data-button');
+  const hideDataButtonElement = document.getElementById('hide-data-button');
+  const tableContainerElement = document.querySelector('#table-container');
+  const chartsRangeInputElement = document.getElementById('charts-range');
+  const loadDataButtonElement = document.getElementById('load-data');
+  const cardsCheckboxElement = document.querySelector('input[name=cards-checkbox]');
+  const gaugesCheckboxElement = document.querySelector('input[name=gauges-checkbox]');
+  const chartsCheckboxElement = document.querySelector('input[name=charts-checkbox]');
+  
+  // DOM elements for sensor readings
+  const cardsReadingsElement = document.querySelector("#cards-div");
+  const gaugesReadingsElement = document.querySelector("#gauges-div");
+  const chartsDivElement = document.querySelector('#charts-div');
+  const tempElement = document.getElementById("temp");
+  const humElement = document.getElementById("hum");
+  const presElement = document.getElementById("pres");
+  const updateElement = document.getElementById("lastUpdate")
+
+  var u = "";
+  
+  // MANAGE LOGIN/LOGOUT UI
+  const setupUI = (user) => {
+    if (user) {
+
+        //toggle UI elements
+      loginElement.style.display = 'none';
+      contentElement.style.display = 'block';
+      authBarElement.style.display ='block';
+      userDetailsElement.style.display ='block';
+      userDetailsElement.innerHTML = user.email;
+  
+      // get user UID to get data from database
+      var uid = user.uid;
+      console.log(uid);
+      u = uid;
+  
+      // Database paths (with user UID)
+      var dbPath = 'UsersData/' + uid.toString() + '/historys';
+      var chartPath = 'UsersData/' + uid.toString() + '/charts/range';
+  
+      // Database references
+      var dbRef = firebase.database().ref(dbPath);
+      var chartRef = firebase.database().ref(chartPath);
+
+      //=========
+      const line1 = document.getElementById('line1')
+      const line2 = document.getElementById('line2')
+      const line3 = document.getElementById('line3')
+      const line4 = document.getElementById('line4')
+      const line5 = document.getElementById('line5')
+      const time2 = document.getElementById('time2')
+      const time3 = document.getElementById('time3')
+      const time4 = document.getElementById('time4')
+      const time5 = document.getElementById('time5')
+      const time6 = document.getElementById('time6')
+      const time7 = document.getElementById('time7')
+      const time8 = document.getElementById('time8')
+      const gps0 = document.getElementById('gps0')
+      const gps1 = document.getElementById('gps1')
+      const unit = document.getElementById('unit')
+
+
+      var i = 0;
+      firebase.database().ref('UsersData/' + uid.toString() + '/config/line').orderByKey().on('child_added', snapshot =>{
+        var jsonData = snapshot.toJSON(); // example: {temperature: 25.02, humidity: 50.20, flag: 1008.48, timestamp:1641317355}
+        document.getElementById('line'+(i+1).toString()).value = jsonData;
+        i+=1
+  
+      });
+
+      var ii = 0;
+      firebase.database().ref('UsersData/' + uid.toString() + '/config/time').orderByKey().on('child_added', snapshot =>{
+        var jsonData = snapshot.toJSON(); // example: {temperature: 25.02, humidity: 50.20, flag: 1008.48, timestamp:1641317355}
+        document.getElementById('time'+(ii+1).toString()).value = jsonData;
+        ii+=1
+  
+      });
+
+      firebase.database().ref('UsersData/' + uid.toString() + '/config/everyhour').orderByKey().on('child_added', snapshot =>{
+        var jsonData = snapshot.toJSON(); // example: {temperature: 25.02, humidity: 50.20, flag: 1008.48, timestamp:1641317355}
+        console.log(jsonData)
+        document.getElementById('everyhour').checked = jsonData;
+  
+      });
+
+      var iii = 0;
+      firebase.database().ref('UsersData/' + uid.toString() + '/config/gps').orderByKey().on('child_added', snapshot =>{
+        var jsonData = snapshot.toJSON(); // example: {temperature: 25.02, humidity: 50.20, flag: 1008.48, timestamp:1641317355}
+        console.log(jsonData)
+        document.getElementById('gps'+iii.toString()).value = jsonData;
+        iii += 1;
+  
+      });
+
+      firebase.database().ref('UsersData/' + uid.toString() + '/config/unit').orderByKey().on('child_added', snapshot =>{
+        var jsonData = snapshot.toJSON(); // example: {temperature: 25.02, humidity: 50.20, flag: 1008.48, timestamp:1641317355}
+        console.log(jsonData)
+        document.getElementById('unit').value = jsonData;
+        document.getElementById('authentication-status').innerHTML = jsonData;
+        
+      });
+
+
+      // CHARTS
+      // Number of readings to plot on charts
+      var chartRange = 0;
+      // Get number of readings to plot saved on database (runs when the page first loads and whenever there's a change in the database)
+      chartRef.on('value', snapshot =>{
+        chartRange = Number(snapshot.val());
+        console.log(chartRange);
+        // Delete all data from charts to update with new values when a new range is selected
+        chartT.destroy();
+        chartH.destroy();
+        // chartP.destroy();
+        // Render new charts to display new range of data
+        chartT = createTemperatureChart();
+        chartH = createHumidityChart();
+        // chartP = createFlagChart();
+        // Update the charts with the new range
+        // Get the latest readings and plot them on charts (the number of plotted readings corresponds to the chartRange value)
+        dbRef.orderByKey().limitToLast(chartRange).on('child_added', snapshot =>{
+          var jsonData = snapshot.toJSON(); // example: {temperature: 25.02, humidity: 50.20, flag: 1008.48, timestamp:1641317355}
+          // Save values on variables
+          var temperature = jsonData.temperature;
+          var humidity = jsonData.humidity;
+          var flag = jsonData.flag;
+          var timestamp = jsonData.timestamp;
+          // Plot the values on the charts
+          plotValues(chartT, timestamp, temperature);
+          plotValues(chartH, timestamp, humidity);
+        //   plotValues(chartP, timestamp, flag);
+        });
+      });
+  
+      // Update database with new range (input field)
+      chartsRangeInputElement.onchange = () =>{
+        chartRef.set(chartsRangeInputElement.value);
+      };
+  
+      //CHECKBOXES
+      // Checbox (cards for sensor readings)
+      cardsCheckboxElement.addEventListener('change', (e) =>{
+        if (cardsCheckboxElement.checked) {
+          cardsReadingsElement.style.display = 'block';
+        }
+        else{
+          cardsReadingsElement.style.display = 'none';
+        }
+      });
+      // Checbox (gauges for sensor readings)
+      gaugesCheckboxElement.addEventListener('change', (e) =>{
+        if (gaugesCheckboxElement.checked) {
+          gaugesReadingsElement.style.display = 'block';
+        }
+        else{
+          gaugesReadingsElement.style.display = 'none';
+        }
+      });
+      // Checbox (charta for sensor readings)
+      chartsCheckboxElement.addEventListener('change', (e) =>{
+        if (chartsCheckboxElement.checked) {
+          chartsDivElement.style.display = 'block';
+        }
+        else{
+          chartsDivElement.style.display = 'none';
+        }
+      });
+  
+      // CARDS
+      // Get the latest readings and display on cards
+      dbRef.orderByKey().limitToLast(1).on('child_added', snapshot =>{
+        var jsonData = snapshot.toJSON(); // example: {temperature: 25.02, humidity: 50.20, flag: 1008.48, timestamp:1641317355}
+        var temperature = jsonData.temperature;
+        var humidity = jsonData.humidity;
+        var flag = jsonData.flag;
+        var timestamp = jsonData.timestamp;
+        // Update DOM elements
+        tempElement.innerHTML = temperature;
+        humElement.innerHTML = humidity;
+        presElement.innerHTML = flag;
+        updateElement.innerHTML = epochToDateTime(timestamp);
+
+        if(flag=="WHITE"){
+            presElement.style.color = "BLACK";
+        }
+        else{
+            presElement.style.color = flag;
+        }
+        
+
+      });
+  
+      // GAUGES
+      // Get the latest readings and display on gauges
+      dbRef.orderByKey().limitToLast(1).on('child_added', snapshot =>{
+        var jsonData = snapshot.toJSON(); // example: {temperature: 25.02, humidity: 50.20, flag: 1008.48, timestamp:1641317355}
+        var temperature = jsonData.temperature;
+        var humidity = jsonData.humidity;
+        var flag = jsonData.flag;
+        var timestamp = jsonData.timestamp;
+        // Update DOM elements
+        var gaugeT = createTemperatureGauge();
+        var gaugeH = createHumidityGauge();
+        gaugeT.draw();
+        gaugeH.draw();
+        gaugeT.value = temperature;
+        gaugeH.value = humidity;
+        updateElement.innerHTML = epochToDateTime(timestamp);
+      });
+  
+      // DELETE DATA
+      // Add event listener to open modal when click on "Delete Data" button
+    //   deleteButtonElement.addEventListener('click', e =>{
+    //     console.log("Remove data");
+    //     e.preventDefault;
+    //     deleteModalElement.style.display="block";
+    //   });
+  
+      // Add event listener when delete form is submited
+      deleteDataFormElement.addEventListener('submit', (e) => {
+        // delete data (readings)
+        dbRef.remove();
+      });
+  
+      // TABLE
+      var lastReadingTimestamp; //saves last timestamp displayed on the table
+      // Function that creates the table with the first 100 readings
+      function createTable(){
+        // append all data to the table
+        var firstRun = true;
+        dbRef.orderByKey().limitToLast(100).on('child_added', function(snapshot) {
+          if (snapshot.exists()) {
+            var jsonData = snapshot.toJSON();
+            console.log(jsonData);
+            var temperature = jsonData.temperature;
+            var humidity = jsonData.humidity;
+            var flag = jsonData.flag;
+            var timestamp = jsonData.timestamp;
+            var content = '';
+            content += '<tr>';
+            content += '<td>' + epochToDateTime(timestamp) + '</td>';
+            content += '<td>' + temperature + '</td>';
+            content += '<td>' + humidity + '</td>';
+            // content += '<td>' + flag + '</td>';
+            if(flag=="WHITE"){
+                content += '<td style="color:' + "BLACK" +'">'+ flag + '</td>';
+            }
+            else{
+                content += '<td style="color:' + flag +'">'+ flag + '</td>';
+            }
+            
+            // <td style="color:red">$100</td>
+            content += '</tr>';
+            $('#tbody').prepend(content);
+            // Save lastReadingTimestamp --> corresponds to the first timestamp on the returned snapshot data
+            if (firstRun){
+              lastReadingTimestamp = timestamp;
+              firstRun=false;
+              console.log(lastReadingTimestamp);
+            }
+          }
+        });
+      };
+  
+      // append readings to table (after pressing More results... button)
+      function appendToTable(){
+        var dataList = []; // saves list of readings returned by the snapshot (oldest-->newest)
+        var reversedList = []; // the same as previous, but reversed (newest--> oldest)
+        console.log("APEND");
+        dbRef.orderByKey().limitToLast(100).endAt(lastReadingTimestamp).once('value', function(snapshot) {
+          // convert the snapshot to JSON
+          if (snapshot.exists()) {
+            snapshot.forEach(element => {
+              var jsonData = element.toJSON();
+              dataList.push(jsonData); // create a list with all data
+            });
+            lastReadingTimestamp = dataList[0].timestamp; //oldest timestamp corresponds to the first on the list (oldest --> newest)
+            reversedList = dataList.reverse(); // reverse the order of the list (newest data --> oldest data)
+  
+            var firstTime = true;
+            // loop through all elements of the list and append to table (newest elements first)
+            reversedList.forEach(element =>{
+              if (firstTime){ // ignore first reading (it's already on the table from the previous query)
+                firstTime = false;
+              }
+              else{
+                var temperature = element.temperature;
+                var humidity = element.humidity;
+                var flag = element.flag;
+                var timestamp = element.timestamp;
+                var content = '';
+                content += '<tr>';
+                content += '<td>' + epochToDateTime(timestamp) + '</td>';
+                content += '<td>' + temperature + '</td>';
+                content += '<td>' + humidity + '</td>';
+                content += '<td>' + flag + '</td>';
+                content += '</tr>';
+                $('#tbody').append(content);
+              }
+            });
+          }
+        });
+      }
+  
+      viewDataButtonElement.addEventListener('click', (e) =>{
+        // Toggle DOM elements
+        tableContainerElement.style.display = 'block';
+        viewDataButtonElement.style.display ='none';
+        hideDataButtonElement.style.display ='inline-block';
+        loadDataButtonElement.style.display = 'inline-block'
+        createTable();
+      });
+  
+      loadDataButtonElement.addEventListener('click', (e) => {
+        appendToTable();
+      });
+  
+      hideDataButtonElement.addEventListener('click', (e) => {
+        tableContainerElement.style.display = 'none';
+        viewDataButtonElement.style.display = 'inline-block';
+        hideDataButtonElement.style.display = 'none';
+      });
+  
+    // IF USER IS LOGGED OUT
+
+    } else{
+      // toggle UI elements
+      loginElement.style.display = 'block';
+      authBarElement.style.display ='none';
+      userDetailsElement.style.display ='none';
+      contentElement.style.display = 'none';
+    }
+  }
+
+  function myFunction() {
+
+    
+    console.log(line1.value);
+    console.log(time1.value);
+
+    var checkedValue = document.getElementById('everyhour');
+    console.log(checkedValue.checked);
+
+
+
+    firebase.database().ref('UsersData/' + u.toString() + '/config/line').set({
+        0 : line1.value,
+        1 : line2.value,
+        2 : line3.value,
+        3 : line4.value,
+      });
+
+      firebase.database().ref('UsersData/' + u.toString() + '/config/time').set({
+        0 : time1.value,
+        1 : time2.value,
+        2 : time3.value,
+        3 : time4.value,
+        4 : time5.value,
+        5 : time6.value,
+        6 : time7.value,
+        7 : time8.value,
+        
+
+      });
+
+      firebase.database().ref('UsersData/' + u.toString() + '/config/everyhour').set({
+        status : checkedValue.checked,
+      });
+
+      firebase.database().ref('UsersData/' + u.toString() + '/config/gps').set({
+        lat : gps0.value,
+        lon : gps1.value
+      });
+
+      firebase.database().ref('UsersData/' + u.toString() + '/config/unit').set({
+        name : unit.value,
+      });
+
+  }
+
+  function myFunction2() {
+
+    document.getElementById('everyhour').checked = false;
+
+    document.getElementById('time1').value = "08:00";
+    document.getElementById('time2').value = "10:00";
+    document.getElementById('time3').value = "13:00";
+    document.getElementById('time4').value = "15:00";
+    document.getElementById('time5').value = "17:00";
+    document.getElementById('time6').value = ":";
+    document.getElementById('time7').value = ":";
+    document.getElementById('time8').value = ":";
+
+  }
+
---
+++ HT-Army_arduino/HT-Army_arduino.ino
@@ -0,0 +1,338 @@
+/*Connect
+HT22
+HT22 <---> D1
+VCC <---> 3V3,
+GND <---> GND
+LED2.4Display
+CS <---> D2 
+RST <---> D3 
+D/C <---> D4 
+MOSI <---> D7 
+SCK <---> D5 
+VCC <---> 3V3,
+GND <---> GND
+*/
+#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
+#include <NTPClient.h> //firebase
+#include <WiFiUdp.h>
+#include <Firebase_ESP_Client.h>
+#include "addons/TokenHelper.h"
+#include "addons/RTDBHelper.h"
+#include "DHT.h"
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
+#include <SPI.h>
+
+#define TFT_CS         D2   //14
+#define TFT_RST        D3   //15
+#define TFT_DC         D4   //32
+#define DHTPIN D1   
+#define DHTTYPE DHT22 
+#define API_KEY "AIzaSyAS18pOu8eEeyTO6iZM80MZQAtsgLVilW0"
+#define DATABASE_URL "https://ht-army-default-rtdb.asia-southeast1.firebasedatabase.app"
+#define USER_EMAIL "ht-army04@htarmy.com"       //---------------------
+#define USER_PASSWORD "12345678"        //---------------------
+#define TIME_SENDFIREBASE 180000     //180000        //---------------------ms
+
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+String uid;
+String databasePath;
+String databasePath2;
+String tempPath = "/temperature";
+String humPath = "/humidity";
+String flagPath = "/flag";  
+String timePath = "/timestamp";
+String parentPath,parentPath2;
+bool firsttime = true;
+float humid,temp,hic,water;
+int train,rest,timestamp;
+String flag,status;
+unsigned long sendDataPrevMillis = 0;
+unsigned long timerDelay = TIME_SENDFIREBASE;
+
+FirebaseJson json,json2;
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org");
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+DHT dht(DHTPIN, DHTTYPE);
+WiFiManager wm;
+
+unsigned long getTime() {
+  timeClient.update();
+  unsigned long now = timeClient.getEpochTime();
+  return now;
+}
+
+void update_firebase(float temp,float humid,String flag){
+  sendDataPrevMillis = millis();
+  timestamp = getTime();
+  // Serial.print ("time: ");
+  // Serial.println (timestamp);
+  parentPath= databasePath + "/" + String(timestamp);
+  json.set(tempPath, temp);
+  json.set(humPath, humid);
+  json.set(flagPath, flag);
+  json.set(timePath, String(timestamp));
+  parentPath2= databasePath2;
+  json2.set(tempPath, temp);
+  json2.set(humPath, humid);
+  json2.set(flagPath, flag);
+  json2.set(timePath, String(timestamp));
+
+  Serial.printf("Set json... %s\n", Firebase.RTDB.setJSON(&fbdo, parentPath.c_str(), &json) ? "ok" : fbdo.errorReason().c_str());
+  Serial.printf("Set2 json... %s\n", Firebase.RTDB.setJSON(&fbdo, parentPath2.c_str(), &json2) ? "ok" : fbdo.errorReason().c_str());
+}
+
+void updateScreen(String flag,float temp,float humid,String status) {
+
+  tft.fillScreen(ST77XX_BLACK);
+  if(status=="online"){
+    tft.setTextColor(ST77XX_GREEN);
+  }
+  else{
+    tft.setTextColor(ST77XX_RED);
+  }
+  
+  tft.setCursor(0, 30);
+  tft.setTextSize(10);
+  tft.print(String(int(floor(temp))));
+  tft.setTextSize(3);
+  tft.print("O");
+
+  tft.setTextColor(ST77XX_YELLOW);
+  tft.setCursor(167, 30);
+  tft.setTextSize(10);
+  tft.print(String(int(floor(humid))));
+  tft.setTextSize(4);
+  tft.print("%");
+
+  tft.setTextSize(4);
+  tft.setCursor(110, 72);
+
+  if(status=="online"){
+    tft.setTextColor(ST77XX_GREEN);
+  }
+  else{
+    tft.setTextColor(ST77XX_RED);
+  }
+  tft.print("." + String(int(10*(temp-floor(temp)))));
+
+  tft.setCursor(270, 72);
+  tft.setTextColor(ST77XX_YELLOW);
+  tft.print("." + String(int(10*(humid-floor(humid)))));
+
+  tft.setCursor(0, 160);
+  tft.setTextSize(7);
+  if(flag=="GREEN"){
+    tft.fillRect(0,140,350,140,ST77XX_GREEN);  //bg
+    tft.setTextColor(ST77XX_BLACK); //text
+    tft.println(" GREEN ");   //7
+  }
+  else if (flag=="RED") {
+    tft.fillRect(0,140,350,140,ST77XX_RED);
+    tft.setTextColor(ST77XX_WHITE);
+    tft.println("  RED  ");   //7
+  } 
+  else if (flag=="YELLOW") {
+    tft.fillRect(0,140,350,140,ST77XX_YELLOW);
+    tft.setTextColor(ST77XX_BLACK);
+    tft.println(" YELLOW ");   //7
+  }
+  else if (flag=="BLACK") {
+    tft.fillRect(0,140,350,140,ST77XX_BLACK);
+    tft.setTextColor(ST77XX_WHITE);
+    tft.println(" BLACK ");   //7
+  }  
+  else if (flag=="WHITE") {
+    tft.fillRect(0,140,350,140,ST77XX_WHITE);
+    tft.setTextColor(ST77XX_BLACK);
+    tft.println(" WHITE ");   //7
+  }  
+}
+
+void setup(){
+  Serial.begin(115200);
+
+  dht.begin();
+
+  tft.init(240, 320);
+  tft.invertDisplay(false);
+  tft.setRotation(3);        
+  tft.fillScreen(ST77XX_BLACK);
+
+  WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP    
+  //wm.resetSettings();
+  wm.setConfigPortalBlocking(false);
+  wm.setConfigPortalTimeout(60);
+  if(wm.autoConnect("AutoConnectAP")){
+      Serial.println("connected...yeey :)");
+  }
+  else {
+      Serial.println("Configportal running");
+  }
+}
+
+void loop(){
+
+  if(WiFi.status()==3 && firsttime == true){
+    timeClient.begin();
+    config.api_key = API_KEY;
+    config.database_url = DATABASE_URL;
+    auth.user.email = USER_EMAIL;
+    auth.user.password = USER_PASSWORD;
+    Firebase.reconnectWiFi(true);
+    fbdo.setResponseSize(4096);
+    config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+    config.max_token_generation_retry = 5;
+    Firebase.begin(&config, &auth);
+    while ((auth.token.uid) == "") {
+      Serial.print('.');
+      delay(1000);
+    }
+    uid = auth.token.uid.c_str();
+    Serial.print("User UID: ");
+    Serial.println(uid);
+    databasePath = "/UsersData/" + uid + "/historys";
+    databasePath2 = "/UsersData/" + uid + "/last";
+
+    firsttime = false;
+
+  }
+  //read DHT
+  humid = dht.readHumidity();
+  temp = dht.readTemperature();
+  if(isnan(humid) || isnan(temp)) {
+    humid = -99;
+    temp = -99;
+    hic = -99;
+  }
+  else{
+    hic = dht.computeHeatIndex(temp, humid, false);
+  }
+  if(hic<27){
+    flag = "WHITE";
+    water = 0.5;
+    train = 60;
+    rest = 0;
+  }
+  else if(hic<32){
+    flag = "GREEN";
+    water = 1.5;
+    train = 50;
+    rest = 10;
+  }
+  else if(hic<41){
+    flag = "YELLOW";
+    water = 1;
+    train = 45;
+    rest = 15;
+  }
+  else if(hic<55){
+    flag = "RED";
+    water = 1;
+    train = 30;
+    rest = 30;
+  }
+  else{
+    flag = "BLACK";
+    water = 1;
+    train = 20;
+    rest = 40;
+  }
+  //find status
+  if(WiFi.status()==3){
+    status = "online";
+    if(Firebase.ready()!=1){
+      status = "notfb";
+    }    
+  }
+  else{
+    status = "offline";
+  }
+  //update screen
+  updateScreen(flag,temp,humid,status);
+  //firebase
+  if (WiFi.status()==3 && Firebase.ready() && (millis() - sendDataPrevMillis > timerDelay || sendDataPrevMillis == 0)){
+    update_firebase(temp,humid,flag);
+  }
+
+  wm.process();
+  delay(5000);
+
+}
+
+
+// #include <ESP8266WiFi.h>          
+// #include <ESP8266WebServer.h>     
+// #include <AutoConnect.h>
+// #include <TridentTD_LineNotify.h>
+
+// ESP8266WebServer Server;          
+// AutoConnect      Portal(Server);
+
+// timeClient.begin();
+// config.api_key = API_KEY;
+// config.database_url = DATABASE_URL;
+// auth.user.email = USER_EMAIL;
+// auth.user.password = USER_PASSWORD;
+// Firebase.reconnectWiFi(true);
+// fbdo.setResponseSize(4096);
+// config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+// config.max_token_generation_retry = 5;
+// Firebase.begin(&config, &auth);
+// while ((auth.token.uid) == "") {
+//   Serial.print('.');
+//   delay(1000);
+// }
+// uid = auth.token.uid.c_str();
+// Serial.print("User UID: ");
+// Serial.println(uid);
+// databasePath = "/UsersData/" + uid + "/history";
+// databasePath2 = "/UsersData/" + uid + "/last";
+
+// for (byte i = 0; i <= 3; i = i + 1) {
+//   if(Firebase.ready()){
+//     if (Firebase.RTDB.getString(&fbdo, "/UsersData/" + uid + "/config/line/" + String(i))) {
+//     // Serial.println(fbdo.dataType());
+//       if (fbdo.dataType() == "string") {
+//         lines[i] = fbdo.stringData();
+//         Serial.println(lines[i]);
+//       }
+//     }
+//     else {
+//       lines[i] = "";
+//       Serial.println(fbdo.errorReason());
+//     }
+//   }
+// }
+
+//check internet
+// Serial.println("WiFi.status: ");    Serial.print(WiFi.status());
+// Serial.println("Firebase.ready: "); Serial.print(Firebase.ready());
+
+
+
+// void rootPage() {
+//   char content[] = "Hello, world";
+//   Server.send(200, "text/plain", content);
+// }
+
+//autoconnect
+// Portal.handleClient();
+
+// //linenotify
+// Serial.println("line-------vvvvv-----vvv--");
+// Serial.println(lines[0]);
+// Serial.println(lines[1]);
+// Serial.println(lines[2]);
+// Serial.println(lines[3]);
+
+// LINE.setToken("m1XN6O5cu7nQSzkMTOOPg7tTxCh4bk4VTBP0XALbRe6"); LINE.notify("xxxxxxx");
+// LINE.setToken(lines[1]); LINE.notify("yyyyyyy");
+
+
+
+
+
